8f785065248878ed76a60f28431fd20a261c0127
Wanzetse Maloba <wanzetseofficial@gmail.com>
2023-09-18 14:54:11 +0300

    Database config file

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://moringa:<dev04>@devo4.j6jba44.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp',
+    development: 'mongodb+srv://moringa:<dev04>@devo4.j6jba44.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp',
+    test: 'mongodb+srv://moringa:<dev04>@devo4.j6jba44.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp',
 }
 module.exports = config;
