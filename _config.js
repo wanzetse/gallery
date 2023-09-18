09d7315527864f79b8848fbf9272ab712f8fd1d2
Wanzetse Maloba <wanzetseofficial@gmail.com>
2023-09-18 15:48:32 +0300

    Database Connection String

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://dev04:<devo4>@devcl.qwafg2x.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp',
-    development: 'mongodb+srv://dev04:<devo4>@devcl.qwafg2x.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp',
-    test: 'mongodb+srv://dev04:<devo4>@devcl.qwafg2x.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp',
+    production: 'mongodb+srv://<wan2>:<wan2>@devcl.qwafg2x.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp',
+    development: 'mongodb+srv://<wan2>:<wan2>@devcl.qwafg2x.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp',
+    test: 'mongodb+srv://<wan2>:<wan2>@devcl.qwafg2x.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp',
 }
 module.exports = config;
